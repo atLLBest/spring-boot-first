82de4430a293e1a20feb0aad23c53d27bb983616
atLLBest <1625273452@qq.com>
2020-11-27 09:55:55 +0800

    接着整git

---
+++ src/main/java/com/atguigu/controller/SurpriseMF.java
@@ -0,0 +1,12 @@
+package com.atguigu.controller;
+
+/**
+ * @author ：Shine
+ * @description：TODO
+ * @date ：2020/11/27 9:54
+ */
+public class SurpriseMF {
+    public static void main(String[] args) {
+        System.out.println("Surprise Mather fucker");
+    }
+}
